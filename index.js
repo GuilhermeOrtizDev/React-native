030a6631c7f9086146ea013a7a6b90c567c0c826
Guilherme Ortiz <guiortizsantos12@gmail.com>
2020-03-06 21:23:10 -0300

    Arquivo drawer att

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './aulas/10-datePicker';
+import App from './aulas/01-index';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
